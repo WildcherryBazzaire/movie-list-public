696c0458500d7b9ac51f944bad9694195cd0c238
Alex <eieghtbitdolphin@gmail.com>
2018-12-19 11:29:02 -1000

    already did

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,38 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="root">
+    <myHeader></myHeader>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
+import MovieTab from './components/MovieTab.vue';
 export default {
-  name: 'app',
+  name: 'root',
+  components: {
+    'myHeader': Header,
+    'movie-tab': MovieTab
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      
     }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+// @font-face {
+//     font-family: 'woomy';
+//     src: url('./assets/Paintball_Beta.otf');
+// }
+* {
+  font-family: "Trebuchet MS", Helvetica, sans-serif;
 }
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+html, body, #root {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  color: rgb(252,252,252);
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="myHeader">
+    <ul>
+      <li v-for="items in content">
+        <h3 class="contents">{{items.name}}</h3>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'my-header',
+  data () {
+    return {
+      content: [
+          {name:"Submit a Movie"},
+          {name:"Delete a Movie"},
+          {name:"Edit a Existing Movie"}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#myHeader {
+  display: flex;
+  width: 100%;
+  height: 8%;
+  background: black;
+}
+ul {
+  display: flex;
+  width: 95%;
+  justify-content: space-between;
+  margin: 0;
+  padding: 0 2.5%;
+}
+li {
+  display: inline-grid;
+}
+.contents {
+  color: rgb(250, 250, 250);
+  margin: auto 0;
+  font-size: 1.65rem;
+}
+</style>
